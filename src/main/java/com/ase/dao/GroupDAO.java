4ffdc5a3352670648356d47e2624c11089a3e3d8
wuchen <wwfyc798@gmail.com>
2015-03-12 18:54:34 -0500

    Add new member
    Add a new user to the group’s user list.

--- src/main/java/com/ase/dao/GroupDAO.java
+++ src/main/java/com/ase/dao/GroupDAO.java
@@ -48,12 +48,23 @@ public class GroupDAO {
 			cursor.close();
 		}
 		List<User> grp_users = new ArrayList<User>();
-		BasicDBList listUsers = (BasicDBList) doc.get("group_users");
+		BasicDBList userList = (BasicDBList) doc.get("group_users");
+		for(Object obj :userList){
+			DBObject userObj = (DBObject)obj; 
+			User user = new User();
+			user.setUserName(userObj.get("user_name").toString());
+			user.setPassWord(userObj.get("password").toString());
+			user.setMember_Title(userObj.get("member_title").toString());
+			user.setUserGroup(grp_name);
+			grp_users.add(user);
+		}
 		
 		
 		grp.setGroupName(doc.get("group_name").toString());
 		grp.setGroupId(doc.get("group_id").toString());
 		grp.setGroupKey(doc.get("group_key").toString());
+		grp.setUsers(grp_users);
+		
 		return grp;
 	}
 	
@@ -61,7 +72,32 @@ public class GroupDAO {
 		
 	}
 	
-	public void addNewMember(User user){
+	public void addNewMember(String grp_name, User user){
+		BasicDBObject query = new BasicDBObject("group_name", grp_name);
+		DBCursor cursor = col.find(query);
+		DBObject doc = null;
+		
+		try{
+			while(cursor.hasNext()){
+				doc = cursor.next();
+			}
+		}finally{
+			cursor.close();
+		}
+		
+		if(doc.get("group_users").equals("")){
+			List<BasicDBObject> users = new ArrayList<BasicDBObject>();
+			doc.put("group_users", users);
+			col.insert(doc);
+		}
+		
+		//Add new user to the group's user list.
+		BasicDBObject push = new BasicDBObject();
+		push.put("$push", new BasicDBObject("group_users",new BasicDBObject("user_name", user.getUserName()).
+				append("password", user.getPassWord()).
+				append("member_title", user.getMember_Title()).
+				append("user_group", user.getUserGroup())));
+		col.update(query, push);
 		
 	}
 	
