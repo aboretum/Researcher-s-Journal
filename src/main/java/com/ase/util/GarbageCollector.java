c5940a163ac429fd9e8595672681254857c9038c
aboretum <wwfyc798@gmail.com>
2015-06-05 15:04:53 -0500

    GarbageCollector - 2
    tested on local machine

--- src/main/java/com/ase/util/GarbageCollector.java
+++ src/main/java/com/ase/util/GarbageCollector.java
@@ -6,8 +6,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
 
-public class GarbageCollector {
-	private static final double CHECK_INTERVAL = 500000;
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.ServletContextAware;
+
+
+
+public class GarbageCollector implements ServletContextAware{
+	@Autowired
+	ServletContext servletContext;
+	
+	private static final double CHECK_INTERVAL = 100000;
 	
 	public void deleteFiles(){
 		File file = new File("/app/resources/images");
@@ -17,13 +27,25 @@ public class GarbageCollector {
 	public void deleteFiles(File file){
 		if(file.isDirectory()){
 			for(File f: file.listFiles()){
-				long diff = new Date().getTime()-file.lastModified();
-				if(diff>60*60*1000)
+				long diff = new Date().getTime()-f.lastModified();
+				
+				if(diff>CHECK_INTERVAL){
+					System.out.println("Deleted "+f.getName());
 					deleteFiles(f);
+				}else{
+					System.out.println("Checked, nothing to delete");
+				}
+					
 			}
 		}else{
 			file.delete();
 		}
 	}
+
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		this.servletContext = servletContext;
+	}
 	
 }
+
